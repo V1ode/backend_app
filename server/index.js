9f4a14b919529b7a9a03caa9c4b7ad878bb36104
V1ode <130359471+V1ode@users.noreply.github.com>
2025-07-11 06:03:23 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const router = require("./routes/index");
 const path = require("path");
 const errorHandler = require("./middleware/ErrorHandlingMiddleware");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 443;
 
 app.use(cors());
 app.use(fileUpload({}));
